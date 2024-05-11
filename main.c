c386306432573c22c53d1e8ad69cbbb006ca98e7
shyamajp <shyamajp@gmail.com>
2024-05-11 22:20:51 +0300

    messy shunting yard

--- main.c
+++ main.c
@@ -27,33 +27,70 @@ double divide(double a, double b)
     return a / b;
 }
 
+void debug(char *operator_stack, char *output_queue)
+{
+    printf("=====================================\n");
+    printf("Operator Stack: %s\n", operator_stack);
+    printf("Output Queue: %s\n", output_queue);
+}
+
 void parseInput(char *string)
 {
     printf("Original String: %s\n", string);
 
     // Shunting yard algorithm
-    char *output_queue = malloc(sizeof(char) * 100);
-    char *output_queue_ptr = output_queue;
-
     char *operator_stack = malloc(sizeof(char) * 100);
     char *operator_stack_ptr = operator_stack;
 
+    char *output_queue = malloc(sizeof(char) * 100);
+    char *output_queue_ptr = output_queue;
+
     for (int i = 0; i < strlen(string); i++)
     {
-        if (string[i] == '+')
+        if (string[i] == '*' || string[i] == '/')
         {
             *operator_stack_ptr = string[i];
             operator_stack_ptr++;
+            debug(operator_stack, output_queue);
         }
+        else if (string[i] == '+' || string[i] == '-')
+        {
+            printf("Top of operator stack: %c\n", *(operator_stack_ptr - 1));
+            // if top of operator stack has higher precedence, pop operators in the operator stack to the output queue
+            while (operator_stack_ptr != operator_stack)
+            {
+                *output_queue_ptr = *(operator_stack_ptr - 1);
+                *(operator_stack_ptr - 1) = '\0';
+                output_queue_ptr++;
+                operator_stack_ptr--;
+                debug(operator_stack, output_queue);
+            }
+
+            *operator_stack_ptr = string[i];
+            operator_stack_ptr++;
+            debug(operator_stack, output_queue);
+        }
+        // Numbers are always pushed to the output queue
         else
         {
             *output_queue_ptr = string[i];
             output_queue_ptr++;
+            debug(operator_stack, output_queue);
         }
     }
 
-    printf("Output Queue: %s\n", output_queue);
-    printf("Operator Stack: %s\n", operator_stack);
+    printf("=====================================\n");
+    // pop the remaining operators from the operator stack to the output queue
+    while (operator_stack_ptr != operator_stack)
+    {
+        *output_queue_ptr = *(operator_stack_ptr - 1);
+        *(operator_stack_ptr - 1) = '\0';
+        output_queue_ptr++;
+        operator_stack_ptr--;
+        debug(operator_stack, output_queue);
+    }
+
+    debug(operator_stack, output_queue);
 }
 
 int main()
@@ -67,7 +104,7 @@ int main()
 
     // printf("Enter a math expression (e.g. 1+1): ");
     // scanf("%s", input);
-    char exp[100] = "3+4+5";
+    char exp[100] = "a+b*c-d";
     parseInput(exp);
     // sscanf(input, "%lf%c%lf", &a, &operator, & b);
 
