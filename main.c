92a207770e106e223577a73bb10c321a8e40b8a2
shyamajp <shyamajp@gmail.com>
2024-05-11 17:24:16 +0300

    allow input with 2 floating numbers

--- main.c
+++ main.c
@@ -3,47 +3,41 @@
 int main()
 {
     // ask for user input e.g. 1+1
-    // 2 single digit numbers only
-    char input[3];
-    printf("Enter a simple math expression (e.g. 1+1): ");
+    // 2 numbers only
+    char operator;
+    double a, b;
+
+    char input[100];
+
+    printf("Enter a math expression (e.g. 1+1): ");
     scanf("%s", input);
+    sscanf(input, "%lf%c%lf", &a, &operator, & b);
 
-    // subtract '0' to get the actual integer value
-    // kinda hacky isnt it?
-    int firstNum = input[0] - '0';
-    int secondNum = input[2] - '0';
-    char operator= input[1];
-
-    printf("First number: %d\n", firstNum);
-    printf("Second number: %d\n", secondNum);
+    printf("First number: %f\n", a);
+    printf("Second number: %f\n", b);
     printf("Operator: %c\n", operator);
 
-    // split string by operator
-
     // calculate result based on operator
-    // it's an integer so result is not always correct :)
-    int result;
+    double result;
     switch (operator)
     {
     case '+':
-        result = firstNum + secondNum;
+        result = a + b;
         break;
     case '-':
-        result = firstNum - secondNum;
+        result = a - b;
         break;
     case '*':
-        result = firstNum * secondNum;
+        result = a * b;
         break;
     case '/':
-        result = firstNum / secondNum;
+        result = a / b;
         break;
     default:
         printf("Invalid operator, exiting...\n");
         return 1;
     }
 
-    // print the result
-    printf("Result: %d\n", result);
-
+    printf("Result: %f\n", result);
     return 0;
 }
