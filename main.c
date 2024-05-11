49d66820fdca6f10e095eb2e64ca8fc2d32c5d1e
shyamajp <shyamajp@gmail.com>
2024-05-11 13:22:45 +0300

    allow operator input

--- main.c
+++ main.c
@@ -5,15 +5,38 @@ int main()
     // ask for user input e.g. 1+1
     // for now, read 2 integers separately
     int firstNum, secondNum;
+    char operator;
 
     printf("Enter first integer: ");
     scanf("%d", &firstNum);
     printf("Enter second integer: ");
     scanf("%d", &secondNum);
+    printf("Enter operator (+, -, *, /): ");
+    scanf(" %c", &operator);
 
     // split string by operator
-    // for now, only support addition
-    int result = firstNum + secondNum;
+
+    // calculate result based on operator
+    // it's an integer so result is not always correct :)
+    int result;
+    switch (operator)
+    {
+    case '+':
+        result = firstNum + secondNum;
+        break;
+    case '-':
+        result = firstNum - secondNum;
+        break;
+    case '*':
+        result = firstNum * secondNum;
+        break;
+    case '/':
+        result = firstNum / secondNum;
+        break;
+    default:
+        printf("Invalid operator, exiting...\n");
+        return 1;
+    }
 
     // print the result
     printf("Result: %d\n", result);
