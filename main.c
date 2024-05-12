94960a950d65975abe3f251457d62bcf36dd2d75
shyamajp <shyamajp@gmail.com>
2024-05-12 21:54:39 +0300

    add parenthesis with bugs

--- main.c
+++ main.c
@@ -44,7 +44,7 @@ void print(struct STACK *stack)
 {
     for (int i = 0; i <= stack->size; i++)
     {
-        printf("%c", stack->elm[i]);
+        printf("%c ", stack->elm[i]);
     }
     printf("\n");
 }
@@ -104,12 +104,14 @@ struct OPERATOR
     enum ASSOCIATIVITY associativity;
 };
 
-struct OPERATOR operators[5] = {
+struct OPERATOR operators[7] = {
     {'+', 1, LEFT},
     {'-', 1, LEFT},
     {'*', 2, LEFT},
     {'/', 2, LEFT},
     {'^', 3, RIGHT},
+    {'(', 0, NONE},
+    {')', 0, NONE},
 };
 
 struct OPERATOR getOperator(char c)
@@ -138,7 +140,6 @@ void parseInput(char *string)
     for (int i = 0; i < strlen(string); i++)
     {
         bool isOperator = false;
-
         for (size_t j = 0; j < sizeof(operators) / sizeof(operators[0]); j++)
         {
             if (string[i] == operators[j].op)
@@ -146,49 +147,94 @@ void parseInput(char *string)
                 isOperator = true;
             }
         }
+        // ✅ left parenthesis: always push to the output queue
+        if (string[i] == '(')
+        {
+            printf("%c | Push token to stack\n", string[i]);
+            push(&operator_stack, string[i]);
+        }
+        // ✅ right parenthesis: pop operators in the operator stack to the output queue until left parenthesis
+        else if (string[i] == ')')
+        {
+            char top_char = last(&operator_stack);
+            while (top_char != '(')
+            {
+                // 🚨 assert: the operator stack is not empty
+                if (isEmpty(&operator_stack))
+                {
+                    printf("Invalid expression, exiting...\n");
+                    exit(1);
+                }
+                char operator= pop(&operator_stack);
+                printf("%c | Add token to output\n", string[i]);
+                push(&output_queue, operator);
 
-        if (isOperator)
+                top_char = last(&operator_stack);
+            }
+
+            // 🚨 assert: there is a left parenthesis at the top of the operator stack
+            if (top_char != '(')
+            {
+                printf("Invalid expression, exiting...\n");
+                exit(1);
+            }
+            printf("%c | Pop stack\n", string[i]);
+            pop(&operator_stack);
+        }
+        // ✅ Operators (+, -, *, /, ^): from the operator stack to the output queue (while ...) + push to the operator stack
+        else if (isOperator)
         {
             struct OPERATOR current = getOperator(string[i]);
-            struct OPERATOR top = getOperator(first(&operator_stack));
+            char top_char = first(&operator_stack);
+            struct OPERATOR top = getOperator(top_char);
 
             // if top of operator stack has higher precedence, pop operators in the operator stack to the output queue
-            while (!isEmpty(&operator_stack) && current.precedence <= top.precedence && current.associativity == LEFT)
+            while (!isEmpty(&operator_stack) && top_char != '(' && current.precedence < top.precedence && current.associativity == LEFT)
             {
+                printf("%c | Pop stack to output\n", string[i]);
                 char operator= pop(&operator_stack);
                 push(&output_queue, operator);
                 top = getOperator(first(&operator_stack));
             }
 
+            printf("%c | Push token to stack\n", string[i]);
             push(&operator_stack, current.op);
         }
-        // Numbers are always pushed to the output queue
+        // ✅ number: to the output queue
         else
         {
+            printf("%c | Add token to output\n", string[i]);
             push(&output_queue, string[i]);
         }
 
         // For debugging purpose
-        printf("=====================================\n");
-        printf("Operator Stack: ");
-        print(&operator_stack);
-        printf("Output Queue: ");
+        printf("output: ");
         print(&output_queue);
+        printf("operator: ");
+        print(&operator_stack);
+        printf("=====================================\n");
     }
 
-    // pop the remaining operators from the operator stack to the output queue
-    while (operator_stack.size != -1)
+    // ✅ all remainings: from the operator stack to the output queue
+    while (!isEmpty(&operator_stack))
     {
+        // 🚨 assert: the operator on top of the stack is not a (left) parenthesis
+        if (last(&operator_stack) == '(')
+        {
+            printf("Invalid expression, exiting...\n");
+            exit(1);
+        }
 
+        printf("%c | Pop stack to output\n", last(&operator_stack));
         char operator= pop(&operator_stack);
         push(&output_queue, operator);
 
         // For debugging purpose
-        printf("=====================================\n");
-        printf("Operator Stack: ");
-        print(&operator_stack);
-        printf("Output Queue: ");
+        printf("output: ");
         print(&output_queue);
+        printf("operator: ");
+        print(&operator_stack);
+        printf("=====================================\n");
     }
 }
 
@@ -204,7 +250,7 @@ int main()
     // printf("Enter a math expression (e.g. 1+1): ");
     // scanf("%s", input);
     // char exp[100] = "a+b*c-d";       // abc*+d-
-    char exp[100] = "3+4*2/(1-5)^2"; // 342*15-2^/+
+    char exp[100] = "3+4*2/(1-5)^2^3"; // 342*15-2^/+
 
     parseInput(exp);
     // sscanf(input, "%lf%c%lf", &a, &operator, & b);
