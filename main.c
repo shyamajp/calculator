2f50717501c7e17e6ed423e83bcb7cb146cfd819
shyamajp <shyamajp@gmail.com>
2024-05-12 18:57:56 +0300

    add precedence

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 double add(double a, double b)
 {
@@ -84,6 +85,32 @@ char pop(struct STACK *stack)
     return c;
 }
 
+struct OPERATOR
+{
+    char op;
+    int precedence;
+};
+
+struct OPERATOR operators[5] = {
+    {'+', 1},
+    {'-', 1},
+    {'*', 2},
+    {'/', 2},
+    {'^', 3},
+};
+
+int getPrecedence(char c)
+{
+    for (size_t i = 0; i < sizeof(operators) / sizeof(operators[0]); i++)
+    {
+        if (c == operators[i].op)
+        {
+            return operators[i].precedence;
+        }
+    }
+    return -1;
+}
+
 void parseInput(char *string)
 {
     printf("Original String: %s\n", string);
@@ -98,27 +125,39 @@ void parseInput(char *string)
     // Shunting yard algorithm
     for (int i = 0; i < strlen(string); i++)
     {
-        if (string[i] == '*' || string[i] == '/')
+        bool isOperator = false;
+        char current = string[i];
+
+        for (size_t j = 0; j < sizeof(operators) / sizeof(operators[0]); j++)
         {
-            push(&operator_stack, string[i]);
-        }
-        else if (string[i] == '+' || string[i] == '-')
-        {
-            char top = first(&operator_stack);
-            printf("Top of operator stack: %c\n", top);
-            // if top of operator stack has higher precedence, pop operators in the operator stack to the output queue
-            while (operator_stack.size != -1)
+            if (current == operators[j].op)
             {
-                char operator= pop(&operator_stack);
-                push(&output_queue, operator);
+                isOperator = true;
             }
-            // finally push the current operator to the operator stack
-            push(&operator_stack, string[i]);
+        }
+
+        if (isOperator)
+        {
+            int current_precedence = getPrecedence(current);
+            char top = first(&operator_stack);
+            int top_precedence = getPrecedence(top);
+
+            // if top of operator stack has higher precedence, pop operators in the operator stack to the output queue
+            if (current_precedence <= top_precedence)
+            {
+                while (operator_stack.size != -1)
+                {
+                    char operator= pop(&operator_stack);
+                    push(&output_queue, operator);
+                }
+            }
+
+            push(&operator_stack, current);
         }
         // Numbers are always pushed to the output queue
         else
         {
-            push(&output_queue, string[i]);
+            push(&output_queue, current);
         }
 
         // For debugging purpose
@@ -156,7 +195,8 @@ int main()
 
     // printf("Enter a math expression (e.g. 1+1): ");
     // scanf("%s", input);
-    char exp[100] = "a+b*c-d";
+    char exp[100] = "a+b*c-d"; // abc*+d-
+
     parseInput(exp);
     // sscanf(input, "%lf%c%lf", &a, &operator, & b);
 
